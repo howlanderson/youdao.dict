a6cf6c246696a800b5aecbc604a9264c630b8ef7
howlanderson <u1mail2me@gmail.com>
2012-04-25 20:52:22 +0800

    version 2 can use youdao translation api so that you no need install lxml pyquery if you request for youdao translation api key if you do not want request youdao api key you can install lxml pyquery, it works like version 1

--- plugin/youdao/youdao.py
+++ plugin/youdao/youdao.py
@@ -1,10 +1,28 @@
-from pyquery import PyQuery as pq
-from lxml import etree
+import urllib2
 import urllib
+import json
 import sys
 
-word = sys.argv[1]
-youdao_url = 'http://dict.youdao.com/search?q=' + word + '&keyfrom=dict.index#q%3Dtest%26keyfrom%3Ddict.index'
-d = pq(url=youdao_url)
-p = d("div#results-contents div.trans-container  ul:first")
-print(p.text().encode("utf-8","ingnore"))
+youdao_key = '1845492630'
+youdao_keyfrom = 'howlanderson'
+
+if not youdao_key:
+    from pyquery import PyQuery as pq
+    from lxml import etree
+    word = sys.argv[1]
+    youdao_url = 'http://dict.youdao.com/search?q=' + word + '&keyfrom=dict.index#q%3Dtest%26keyfrom%3Ddict.index'
+    d = pq(url=youdao_url)
+    p = d("div#results-contents div.trans-container  ul:first")
+    print(p.text().encode("utf-8","ingnore"))
+else:
+    word = sys.argv[1]
+    youdao_url = 'http://fanyi.youdao.com/openapi.do?keyfrom=' + youdao_keyfrom + '&key=' + youdao_key + '&type=data&doctype=json&version=1.1&q=' + word
+    req = urllib2.Request(url = youdao_url)
+    f = urllib2.urlopen(req)
+    json_data = f.read()
+    json_object = json.loads(json_data)
+    if not json_object['errorCode']: 
+        print (json_object['basic']['explains'][0]).encode('utf-8', 'ignore');
+        print (json_object['basic']['explains'][1]).encode('utf-8', 'ignore');
+    else:
+        print 'api return error'
